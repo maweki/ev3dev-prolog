28501aa7197f44139e03e1ecdd88f25db0794bc4
Mario Wenzel <maweki@gmail.com>
2017-09-15 11:07:16 +0200

    servo motor no longer "works"

--- lib/subsystems/servo-motor.pl
+++ lib/subsystems/servo-motor.pl
@@ -1,9 +1,8 @@
-:- multifile servo_motor/1, device_path/2.
-:- dynamic   servo_motor/1.
 :- ['../fileaccess.pl'].
 
 
 servo_motor(_) :- false.
 
+% this no longer works
 device_path(Port, DevicePath) :-
   device_path(Port, '/sys/class/servo-motor/motor', DevicePath).
