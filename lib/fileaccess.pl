189bc45eeb7b6027a162947cf0162c43d0f876d4
Mario Wenzel <maweki@gmail.com>
2017-09-15 11:06:41 +0200

    removed unused predicates

--- lib/fileaccess.pl
+++ lib/fileaccess.pl
@@ -36,39 +36,3 @@ subsystem_detect(Port, Type, Path, Prefix) :-
       (retract(subsystem_detect(Port_, Type_, Path, Prefix)), fail)
   ),
   Port_ = Port, Type_ = Type.
-
-%% the following stuff is potentially not needed anymore
-device_path(Port, Basepath, DevicePath) :-
-  port_symbol(Port, Symbol),
-  atomic_concat(Basepath, '*/address', Template),
-  expand_(Template, AddressFile),
-  file_read(AddressFile, Content), Content = Symbol, !,
-  file_directory_name(AddressFile, DevicePath),
-  asserta(
-    device_path(Port, Basepath, DevicePath) :-
-      exists_directory(DevicePath);
-      (retract(device_path(Port, Basepath, DevicePath)), fail)
-  ).
-
-detect_port(Port, Prefix, DriverName) :-
-  atomic_concat(Prefix, '*/', Template),
-  expand_(Template, Basepath),
-  atomic_concat(Basepath, '/address', AddressFile),
-  atomic_concat(Basepath, '/driver_name', DriverFile),
-  file_read(DriverFile, DriverName),
-  file_read(AddressFile, Port),
-  asserta(
-    detect_port(Port, Prefix, DriverName) :-
-      exists_directory(Basepath);
-      (retract(detect_port(Port, Prefix, DriverName)), fail)
-  ).
-
-%! expand_(+F:Wildcard, -E:Path) is nondet
-%
-% expand a wildcard pattern and return one match
-%
-% @arg F wildcard pattern
-% @arg E single path matching wildcard
-expand_(F, E) :-
-  expand_file_name(F, L),
-  member(E, L).
