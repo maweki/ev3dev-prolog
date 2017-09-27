36d7973634d6fa36b885a20fe29fae33506b4683
Mario Wenzel <maweki@gmail.com>
2017-09-27 14:37:35 +0200

    fixed examples

--- examples/braitenberg.pl
+++ examples/braitenberg.pl
@@ -26,22 +26,22 @@ braitenberg1c :-
 % Je mehr Licht desto schneller das Vehikel, Motor Sensor Abhänigkeit
 % Für braitenberg2b, am Roboter Ports von Motoren ODER Sensoren tauschen
 braitenberg2 :-
-  col_ambient(port2, LightR), motor_run(portB, LightR),
-  col_ambient(port3, LightL), motor_run(portC, LightL),
+  col_ambient(in2, LightR), motor_run(outB, LightR),
+  col_ambient(in3, LightL), motor_run(outC, LightL),
   braitenberg2.
 
 % Je mehr Licht desto langsamer das Vehikel, Motor Sensor Abhänigkeit
 % Für braitenberg3b, am Roboter Ports von Motoren ODER Sensoren tauschen
 braitenberg3 :-
-  col_ambient(port2, LightR), SpeedR is max(0, 50 - LightR),
-  col_ambient(port3, LightL), SpeedL is max(0, 50 - LightL),
-  motor_run(portB, SpeedR), motor_run(portC, SpeedL),
+  col_ambient(in2, LightR), SpeedR is max(0, 50 - LightR),
+  col_ambient(in3, LightL), SpeedL is max(0, 50 - LightL),
+  motor_run(outB, SpeedR), motor_run(outC, SpeedL),
   braitenberg3.
 
 %Je mehr Licht umso schneller bis Licht > 50, dann je mehr Licht langsamer
 braitenberg4 :-
-  col_ambient(port2, LightR),
-  col_ambient(port3, LightL),
+  col_ambient(in2, LightR),
+  col_ambient(in3, LightL),
   ((LightR < 50,
     SpeedR is LightR^2 / 100.0);
     SpeedR is 50 - LightR^2 / 100.0
@@ -50,16 +50,16 @@ braitenberg4 :-
     SpeedL is LightL^2 / 100.0);
     SpeedL is 50 - LightL^2 / 100.0
   ),
-  motor_run(portB, SpeedR), motor_run(portC, SpeedL),
+  motor_run(outB, SpeedR), motor_run(outC, SpeedL),
 braitenberg4.
 
 % Vehikel fährt mit konstanter Geschwindigkeit(100), bis Licht > 60, dann Motor(en) (fast) volle Leistung rückwärts
 braitenberg5 :-
-  col_ambient(port2, LightR),
-  col_ambient(port3, LightL),
+  col_ambient(in2, LightR),
+  col_ambient(in3, LightL),
   ((LightR > 50, SpeedR is 100);
     SpeedR is 40),
   ((LightL > 50, SpeedL is 100);
     SpeedL is 40),
-  motor_run(portB, SpeedL), motor_run(portC, SpeedR), sleep(1),
+  motor_run(outB, SpeedL), motor_run(outC, SpeedR), sleep(1),
 braitenberg5.
--- examples/obstacleavoidance.pl
+++ examples/obstacleavoidance.pl
@@ -1,12 +1,8 @@
-consult('../ev3.pl').
-
-ev3_large_motor(portB).
-ev3_large_motor(portC).
-ultrasonic_sensor(port1).
+:- ['../ev3.pl'].
 
 obstacle_avoidance :-
-  ((us_dist_cm(port1, Dist), Dist > 50,
-    speed_sp(portB, 50, true), speed_sp(portC, 50, true)
+  ((us_dist_cm(in4, Dist), Dist > 50,
+    motor_run(outB, 20), motor_run(outC, 20)
   );
-  speed_sp(portB, 0)),
+  (motor_stop(outC), motor_run(outB, 20, 360))),
   obstacle_avoidance.
