5022d227f6b5377a3f4a19408c902ee246b93dfb
Mario Wenzel <maweki@gmail.com>
2017-09-28 13:31:23 +0200

    removed params

--- ev3.pl
+++ ev3.pl
@@ -1,23 +1,22 @@
 :- ['ev3_base.pl'].
 
 % As done by Nalepa
-set_robot(WheelCircumference, AxleLength, LeftMotor, RightMotor, Reverse) :-
-  nonvar(WheelCircumference),
+set_robot(WheelDiameter, AxleLength, LeftMotor, RightMotor) :-
+  nonvar(WheelDiameter),
   nonvar(AxleLength),
   nonvar(LeftMotor),
   nonvar(RightMotor),
-  nonvar(Reverse),
   tacho_motor(LeftMotor),
   tacho_motor(RightMotor),
   retractall(
     robot(_, _, _, _, _)
   ),
   asserta(
-    robot(WheelCircumference, AxleLength, LeftMotor, RightMotor, Reverse)
+    robot(WheelDiameter, AxleLength, LeftMotor, RightMotor)
   ).
 
 stop :-
-  robot(_, _, LM, RM, _),
+  robot(_, _, LM, RM),
   motor_stop(LM),
   motor_stop(RM).
 
@@ -26,28 +25,28 @@ stop :-
 
 go(Speed) :-
   Speed \= 0,
-  robot(_, _, LM, RM, _),
+  robot(_, _, LM, RM),
   motor_run(LM, Speed),
   motor_run(RM, Speed).
 
 go(Speed, Angle) :-
   Speed \= 0, Angle \= 0,
-  robot(_, _, LM, RM, _),
+  robot(_, _, LM, RM),
   thread_create(motor_run(LM, Speed, Angle), Id1, []),
   thread_create(motor_run(RM, Speed, Angle), Id2, []),
   thread_join(Id1, true),
   thread_join(Id2, true),!.
 
 go_cm(Speed, Distance) :-
-  robot(WC, _, _, _, _),
-  Angle is round((Distance*360)/(pi*WC)),
+  robot(WD, _, _, _),
+  Angle is round((Distance*360)/(pi*WD)),
   go(Speed,Angle).
 
 turn(Speed, Angle) :-
   gyro_sensor(Port).
 
 turn(Speed, Angle) :-
-  robot(WD, AL, LM, RM, _),
+  robot(WD, AL, LM, RM),
   MAngle is round(AL/WD*Angle),
   NegMAngle is -MAngle,
   thread_create(motor_run(LM, Speed, MAngle), Id1, []),
