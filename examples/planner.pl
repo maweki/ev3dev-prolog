f38620bb297996ffeac26c5803805f2bf51540fe
Daniel Schmelzer <daniel.schmelzer@stud.htwk-leipzig.de>
2017-10-18 13:35:33 +0200

    move(turn_left, [],[]) and  move(turn_right, [],[]) implemented

--- examples/planner.pl
+++ examples/planner.pl
@@ -2,11 +2,11 @@
 %%%
 %%% This is one of the example programs from the textbook:
 %%%
-%%% Artificial Intelligence: 
+%%% Artificial Intelligence:
 %%% Structures and strategies for complex problem solving
 %%%
 %%% by George F. Luger and William A. Stubblefield
-%%% 
+%%%
 %%% Corrections by Christopher E. Davis (chris2d@cs.unm.edu)
 %%%
 %%% These programs are copyrighted by Benjamin/Cummings Publishers.
@@ -14,19 +14,19 @@
 %%% We offer them for use, free of charge, for educational purposes only.
 %%%
 %%% Disclaimer: These programs are provided with no warranty whatsoever as to
-%%% their correctness, reliability, or any other property.  We have written 
+%%% their correctness, reliability, or any other property.  We have written
 %%% them for specific educational purposes, and have made no effort
-%%% to produce commercial quality computer programs.  Please do not expect 
+%%% to produce commercial quality computer programs.  Please do not expect
 %%% more of them then we have intended.
 %%%
 %%% This code has been tested with SWI-Prolog (Multi-threaded, Version 5.2.13)
 %%% and appears to function as intended.
 
 :- [adts].
-plan(State, Goal, _, Moves) :- 	equal_set(State, Goal), 
+plan(State, Goal, _, Moves) :- 	equal_set(State, Goal),
 				write('moves are'), nl,
 				reverse_print_stack(Moves).
-plan(State, Goal, Been_list, Moves) :- 	
+plan(State, Goal, Been_list, Moves) :-
 				move(Name, Preconditions, Actions),
 				conditions_met(Preconditions, State),
 				change_state(State, Actions, Child_state),
@@ -47,29 +47,37 @@ member_state(S, [H|_]) :- 	equal_set(S, H).
 member_state(S, [_|T]) :- 	member_state(S, T).
 
 reverse_print_stack(S) :- 	empty_stack(S).
-reverse_print_stack(S) :- 	stack(E, Rest, S), 
+reverse_print_stack(S) :- 	stack(E, Rest, S),
 				reverse_print_stack(Rest),
 		 		write(E), nl.
 
 
 /* sample moves */
 
-move(pickup(X), [handempty, clear(X), on(X, Y)], 
-		[del(handempty), del(clear(X)), del(on(X, Y)), 
+move(move_if_free, [position(X,Y), orientation(DX, DY), not_clear(DX),not_clear(DY)], [del(not_clear(DX)),del(not_clear(DY)),del(position(X,Y)), add(position(X+DX, Y+DY))]).
+move(turn_right, [orientation(0,1)], [del(orientation(0,1)), add(orientation(1,0))]).
+move(turn_left, [orientation(0,1)], [del(orientation(0,1)), add(orientation(-1,0))]).
+
+move(pickup(X), [handempty, clear(X), on(X, Y)],
+		[del(handempty), del(clear(X)), del(on(X, Y)),
 				 add(clear(Y)),	add(holding(X))]).
 
-move(pickup(X), [handempty, clear(X), ontable(X)], 
-		[del(handempty), del(clear(X)), del(ontable(X)), 
+move(pickup(X), [handempty, clear(X), ontable(X)],
+		[del(handempty), del(clear(X)), del(ontable(X)),
 				 add(holding(X))]).
 
-move(putdown(X), [holding(X)], 
-		[del(holding(X)), add(ontable(X)), add(clear(X)), 
+move(putdown(X), [holding(X)],
+		[del(holding(X)), add(ontable(X)), add(clear(X)),
 				  add(handempty)]).
 
-move(stack(X, Y), [holding(X), clear(Y)], 
+move(stack(X, Y), [holding(X), clear(Y)],
 		[del(holding(X)), del(clear(Y)), add(handempty), add(on(X, Y)),
 				  add(clear(X))]).
 
 go(S, G) :- plan(S, G, [S], []).
 test :- go([handempty, ontable(b), ontable(c), on(a, b), clear(c), clear(a)],
  	          [handempty, ontable(c), on(a,b), on(b, c), clear(a)]).
+
+% pickup(a) -> [ontable(b), ontable(c), clear(c), clear(b), holding(a)]
+% pickup(c) -> [ontable(b), on(a, b), clear(a), holding(c)]
+% pickup(a) -> putdown(a) -> [ontable(b), ontable(c), clear(c), clear(b), ontable(a), clear(a), handempty]
